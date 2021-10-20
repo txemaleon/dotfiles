859836cc46d5b90d33db284d950d17e581095dbb
Txema León <soy@txemaleon.com>
2021-10-20 18:30:50 +0200

    Adds Migration Script

---
+++ tools/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+for f in profile fixpackrc gitconfig gitignore inputrc npmrc slate tmux.conf vimrc zshrc;
+do
+	rm -rf ~/.$f;
+	echo "Linking $f"
+	ln -s ~/.config/dotfiles/$f ~/.$f;
+done
