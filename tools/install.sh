ec7b0d622fa6c34d777d88afea3336f167821c3d
Txema León <soy@txemaleon.com>
2022-04-06 09:42:12 -0300

    Autolink config files

--- tools/install.sh
+++ tools/install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 
-for f in profile fixpackrc gitconfig gitignore inputrc npmrc tmux.conf vimrc zshrc;
+for f in config/*;
 do
 	rm -rf ~/.$f;
 	echo "Linking $f"
-	ln -s ~/.config/dotfiles/$f ~/.$f;
+	ln -s ~/.config/dotfiles/config/$f ~/.$f;
 done
