13610b51c55e1f92313367d17f2444b7348e2cbc
Txema León <soy@txemaleon.com>
2022-07-14 16:28:48 +0200

    Removes legacy code

--- install/installer.sh
+++ install/installer.sh
@@ -3,26 +3,24 @@
 # Ask for the administrator password upfront
 sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+while true; do
+	sudo -n true
+	sleep 60
+	kill -0 "$$" || exit
+done 2>/dev/null &
 
 # Install dotfiles
 DOTFILES=$(dirname ${0:a:h})
-for FILE in $DOTFILES/config/*;
-do
-	f=$(basename $FILE);
-	rm -rf ~/.$f;
+for FILE in $DOTFILES/config/*; do
+	f=$(basename $FILE)
+	rm -rf ~/.$f
 	echo "Linking $FILE => .$f"
-	ln -s $FILE ~/.$f;
+	ln -s $FILE ~/.$f
 done
 
 # Install HomeBrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew bundle
-
-# # Install dotfiles, Terminal config, and Sublime Text Configs
-# mkdir -p ~/.config/
-# git clone git@github.com:txemaleon/dotfiles.git ~/.config/dotfiles
-# ~/.config/dotfiles/tools/install.sh
 
 # Link mackup
 ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/.config/.mackup ~/.mackup
