d5a3030c8c02fc5af9e6108ed14bf972c5d78188
Txema León <soy@txemaleon.com>
2022-09-28 22:22:32 +0200

    Adds `id_rsa` to macos keychain on setup

--- install/gitconfig.sh
+++ install/gitconfig.sh
@@ -5,4 +5,5 @@ git config --global gpg.format ssh
 
 if [[ -f ~/.ssh/id_rsa.pub ]]; then
 	git config --system user.signingkey "$(cat ~/.ssh/id_rsa.pub)"
+	ssh-add --apple-use-keychain ~/.ssh/id_rsa
 fi
