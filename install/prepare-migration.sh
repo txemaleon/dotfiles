e9c18638a3aae9a0f96e1b8a7ba6c623794c616f
Txema León <soy@txemaleon.com>
2023-01-27 11:15:15 -0300

    Removes npm aliases in favor of `ni` package

--- install/prepare-migration.sh
+++ install/prepare-migration.sh
@@ -4,7 +4,7 @@ clear
 
 function dumpBrew { brew bundle dump --force; }
 
-function dumpNPM { find $(npm root -g) -type d -depth 1 -print0 | sort -z | xargs -r0 basename >Npmfile; }
+function dumpNPM { npm list -pg --depth=0 | sed -e "s/├── //" -e "s/│ //" -e "s/└── //" -e "s~$(npm root -g)\/~~" -e "/^-$/d" -e "/\/usr\/local/d" | sed -e "/^(\s*)$/d" | sort -z | tr -d '\000' >Npmfile; }
 
 function dumpMackup { mackup backup --force; }
 
