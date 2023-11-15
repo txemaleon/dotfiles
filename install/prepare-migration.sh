aae9a8f17fe094c767db8a73d3eaa2ae7b07852c
Txema León <soy@txemaleon.com>
2023-11-15 16:07:50 -0300

    fix: removes mackup backup after dumping install files

--- install/prepare-migration.sh
+++ install/prepare-migration.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
 
-clear
-
 function dumpBrew { brew bundle dump --force; }
 
 function dumpNPM { npm list -pg --depth=0 | sed -e "s/├── //" -e "s/│ //" -e "s/└── //" -e "s~$(npm root -g)\/~~" -e "/^-$/d" -e "/\/usr\/local/d" | sed -e "/^(\s*)$/d" | sort -z | tr -d '\000' >Npmfile; }
 
-function dumpMackup { mackup backup --force; }
+function dumpMackup {
+	mackup backup --force
+	mackup uninstall --force
+}
 
 function prepare {
 	clear
@@ -15,4 +16,5 @@ function prepare {
 	dumpMackup
 }
 
+clear
 prepare
