c1d11a2ec86e6f257ff81b9529971ac562190bba
Txema León <soy@txemaleon.com>
2025-04-29 17:03:51 +0200

    feat: update Brewfile to remove unnecessary linking for ffmpeg@6, add new applications, and modify existing MAS entries

--- install/prepare-migration.sh
+++ install/prepare-migration.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env zsh
 
-function dumpBrew { brew bundle dump --force; }
+function dumpBrew { brew bundle dump --force --no-vscode; }
 
 function dumpNPM { npm list -pg --depth=0 | sed -e "s/├── //" -e "s/│ //" -e "s/└── //" -e "s~$(npm root -g)\/~~" -e "/^-$/d" -e "/\/usr\/local/d" | sed -e "/^(\s*)$/d" | sort -z | tr -d '\000' >Npmfile; }
 
 function dumpMackup {
 	mackup backup --force
-	mackup uninstall --force
 }
 
 function prepare {
