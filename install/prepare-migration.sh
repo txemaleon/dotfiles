1107a96b74b9434af32e42ede74055370c9e4531
Txema León <soy@txemaleon.com>
2025-04-29 17:05:45 +0200

    fix: update dumpNPM function to remove the first line from npm list output for cleaner Npmfile generation

--- install/prepare-migration.sh
+++ install/prepare-migration.sh
@@ -2,7 +2,7 @@
 
 function dumpBrew { brew bundle dump --force --no-vscode; }
 
-function dumpNPM { npm list -pg --depth=0 | sed -e "s/├── //" -e "s/│ //" -e "s/└── //" -e "s~$(npm root -g)\/~~" -e "/^-$/d" -e "/\/usr\/local/d" | sed -e "/^(\s*)$/d" | sort -z | tr -d '\000' >Npmfile; }
+function dumpNPM { npm list -pg --depth=0 | sed '1d' | sed -e "s/├── //" -e "s/│ //" -e "s/└── //" -e "s~$(npm root -g)\/~~" -e "/^-$/d" -e "/\/usr\/local/d" | sed -e "/^(\s*)$/d" | sort -z | tr -d '\000' >Npmfile; }
 
 function dumpMackup {
 	mackup backup --force
